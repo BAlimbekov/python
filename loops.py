cfe323d3131e8b75baa8d585c33a6d6ba7a87b44
BAlimbekov <alimbekovbekmambet@gmail.com>
2023-03-19 22:39:09 -0500

    Messing around with Python

---
+++ loops.py
@@ -0,0 +1,3 @@
+names = ["Harry", "Bek", "Mochi"]
+for name in names:
+    print(name)
---
+++ sequences.py
@@ -0,0 +1,8 @@
+#comment or pseudocode Define a list of names
+
+names = ["Harry", "Bek", "Mochi"]
+
+
+names.append("Draco")
+names.sort()
+print(names)
